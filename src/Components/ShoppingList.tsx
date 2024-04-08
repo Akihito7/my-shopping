05a44b77696a1ac9219511db832cc4e9ad92cd52
Akihito7 <akihitopro7@gmail.com>
2024-04-08 18:50:46 -0300

    usando o snapshot pra criar evento de listener e atualizar ereal time os produtos

--- src/Components/ShoppingList.tsx
+++ src/Components/ShoppingList.tsx
@@ -1,4 +1,4 @@
-import { DocumentData, collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import { FlatList } from "native-base";
 import { db } from "../configs/firebaseConfig";
 import { useEffect, useState } from "react";
@@ -6,39 +6,37 @@ import { CardItem } from "./CardItem";
 
 
 type PropsProduct = {
-    id : string;
-    name : string;
-    done : boolean,
-    quantity : number,
+    id: string;
+    name: string;
+    done: boolean,
+    quantity: number,
 }
 
 export function ShoppingList() {
 
     const [products, setProducts] = useState<PropsProduct[]>([]);
 
-    async function getProducts() {
-        try {
-            const collectionOfProducts = await getDocs(collection(db, "products"));
-
-            const data = collectionOfProducts.docs.map(product => {
-
-                return {
-                    id: product.id,
-                    name: product.data().name,
-                    done: product.data().done,
-                    quantity: product.data().quantity
-                };
-            })
-
-            setProducts(data)
-        } catch (error) {
-
-        }
-    }
 
     useEffect(() => {
-        getProducts()
+
+        const unsubscribe = onSnapshot(collection(db, "products"), (querySnapshot) => {
+
+            const data = querySnapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    name: doc.data().name,
+                    done: doc.data().done,
+                    quantity: doc.data().quantity
+                }
+            })
+
+            setProducts(data);
+        })
+
+        return () => unsubscribe();
+
     }, [])
+
 
     return (
         <FlatList
@@ -53,7 +51,7 @@ export function ShoppingList() {
                 />
             )}
 
-            
+
             px={4}
             mb={4}
         />
